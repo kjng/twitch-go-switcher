13859c28601739b09f2a698193e1766abe1ebe9e
Kevin Jang <pianistkevinjang@gmail.com>
2017-10-11 11:10:28 -0400

    Enable extension by default

--- redirect.js
+++ redirect.js
@@ -1,5 +1,15 @@
 chrome.storage.sync.get('twitch_go_switcher', (items) => {
-    if (items.twitch_go_switcher === 'enable') {
+    var status = items.twitch_go_switcher;
+
+    // If just installed without explicitly enabling in the extension popup, enable by default
+    if (!status) {
+        chrome.storage.sync.set({
+            twitch_go_switcher: 'enable'
+        });
+        redirectToTwitchGo();
+    }
+
+    if (status === 'enable') {
       console.log('Redirecting to twitch go.');
       redirectToTwitchGo();
     }
